e67b5ddcb7828b5a08421a3b4a7051436e46f67e
Khushboo-ms <56860877+Khushboo-ms@users.noreply.github.com>
2020-05-06 15:46:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import $ from "jquery";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+class App extends React.Component {
+  setMovement(e) {
+    var p = { x: e.movementX, y: e.movementY },
+      mPos = { x: e.screenX, y: e.screenY },
+      x = mPos.x - p.x - 100,
+      y = mPos.y - p.y - 100;
+    console.log(x, y);
+
+    $(".moon").css({
+      left: x,
+      top: y,
+      backgroundPosition: -x + "px " + -y + "px",
+    });
+  }
+  render() {
+    return (
+      <div className="App" onMouseMove={(e) => this.setMovement(e)}>
+        <div className="moon"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
