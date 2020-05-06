bd73ee2d678f03009b1091270fa67a047fdd3e5a
Khushboo-ms <56860877+Khushboo-ms@users.noreply.github.com>
2020-05-06 15:57:12 +0530

    Updates

---
+++ precache-manifest.4c5e32807914e2c478bf1e88ad0167cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59329a37481d09386f5af3c62aeb90c2",
+    "url": "/move-stars/index.html"
+  },
+  {
+    "revision": "f6a90e706c1af2764605",
+    "url": "/move-stars/static/css/main.ce182706.chunk.css"
+  },
+  {
+    "revision": "dede4db600bd539307d6",
+    "url": "/move-stars/static/js/2.cf7016a4.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/move-stars/static/js/2.cf7016a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a90e706c1af2764605",
+    "url": "/move-stars/static/js/main.f52c7169.chunk.js"
+  },
+  {
+    "revision": "0d10239e73c2c6e4c6ba",
+    "url": "/move-stars/static/js/runtime-main.8ba04be8.js"
+  }
+]);
